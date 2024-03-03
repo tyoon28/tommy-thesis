30b2693f32282d1a869f350e69bfb2bc370036d9
dumblittlebboy <tyoon@reed.edu>
2024-03-02 17:55:00 -0800

    s

--- analyze_orca_output.py
+++ analyze_orca_output.py
@@ -11,7 +11,7 @@ def main():
 
         finalDf,pca = PCA_gdd(ldirs)
         print(f'making PCA plots for {r}')
-        plot_PCA_gdd(finalDf,pca,f'{r}_PCA_gdd')
+        plot_PCA_gdd(finalDf,f'{r}_PCA_gdd')
 
         print(f'doing cholesterol plots for {r}')
         graphlets_cholesterol_pca(r)
