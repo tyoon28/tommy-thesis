d73f1475d3aa2a97461abe863d82aa2b37be05db
dumblittlebboy <tyoon@reed.edu>
2024-04-12 11:40:40 -0700

    a

--- graphlets.py
+++ graphlets.py
@@ -264,7 +264,7 @@ def graphlets_cholesterol_pca(r):
     '''analyze orca output. r is R1-R3. Orca output must be in ../orca/output/{r}-<x>-closed'''
     #PCA
     if r == 'all':
-        ldirs = [f'../orca/output/{r}-15-closed-contact',f'../orca/output/{r}-30-closed-contact' for r in ['15','30']]
+        ldirs = [f'../orca/output/{r}-{c}-closed-contact' for r in ['R1','R2','R3'] for c in ['15,30']]
     else:
         ldirs = [f'../orca/output/{r}-15-closed-contact',f'../orca/output/{r}-30-closed-contact']
     gdds = []
@@ -701,7 +701,7 @@ def node_PCA_windowed(r,ldirs,output=False):
     '''for each node do logistic regression to fit node distance ~ cholesterol. output p values.'''
 
     if r == 'all':
-        ldirs = [f'../orca/output/{r}-15-closed-contact',f'../orca/output/{r}-30-closed-contact' for r in ['15','30']]
+        ldirs = [f'../orca/output/{r}-{c}-closed-contact' for r in ['R1','R2','R3'] for c in ['15,30']]
     else:
         ldirs = [f'../orca/output/{r}-15-closed-contact',f'../orca/output/{r}-30-closed-contact']
 
