8109f06b1ac01db9002b51fb7526dfae77beecf6
dumblittlebboy <tyoon@reed.edu>
2024-04-20 17:01:36 -0700

    d

--- analyze_orca_output.py
+++ analyze_orca_output.py
@@ -48,14 +48,14 @@ def main():
     # print(f'calculating variable importance for all')
     # logistic_selection(df,r)
 
-    print(f'doing cholesterol plots for all')
-    graphlets_cholesterol_pca('all',to_csv=True)
+    # print(f'doing cholesterol plots for all')
+    # graphlets_cholesterol_pca('all',to_csv=True)
 
-    # print(f'doing individual nodes for all')
-    # node_pca_analysis(r,output=True)
+    # # print(f'doing individual nodes for all')
+    # # node_pca_analysis(r,output=True)
 
-    print(f'doing individual nodes windowed for all')
-    node_PCA_windowed(r,output=True,to_csv=True)
+    # print(f'doing individual nodes windowed for all')
+    # node_PCA_windowed(r,output=True,to_csv=True)
 
 
     # TODO: which nodes are highly predictive of cholesterol condition?
