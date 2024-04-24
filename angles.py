854ded77151faf62bb1e0601953dc568c95b4826
dumblittlebboy <tyoon@reed.edu>
2024-04-23 23:32:17 -0700

    sd

--- angles.py
+++ angles.py
@@ -27,34 +27,42 @@ def main():
                     xtcs.append(f'{r}-{i}-closed/'+file)
             xtcs.sort(key=lambda x: int(x.split('-')[1]))
             u = mda.Universe(f'{r}-{i}-closed/{r}-0-start-membrane-3JYC.pdb',*xtcs,continuous=True)
-            protein = u.select_atoms(selstring)
-            for ts in tqdm.tqdm(u.trajectory):
-                frame = u.trajectory.frame
-                poss = protein.positions
-                vectors = np.zeros((int(len(poss)/2),2))
+            print(i,r,len(u.atoms))
+    #         protein = u.select_atoms(selstring)
+    #         for ts in tqdm.tqdm(u.trajectory[500:1000]):
+    #             frame = u.trajectory.frame
+    #             poss = protein.positions
+    #             vectors = np.zeros((int(len(poss)/2),2))
                 
-                for j,p in enumerate(poss.reshape((int(len(poss)/2), 2,3))):
-                    vectors[j] = (p[0] - p[1])[:2]
-                distmat = 1 - cdist(vectors, vectors, metric='cosine')
-                flattened = distmat[np.triu_indices_from(distmat, k=1)]
-                angle = np.mean(abs(flattened))
-                angles.append(angle)
-                iters += 1
+    #             for j,p in enumerate(poss.reshape((int(len(poss)/2), 2,3))):
+    #                 vectors[j] = (p[0] - p[1])[:2]
+    #             distmat = 1 - cdist(vectors, vectors, metric='cosine')
+    #             flattened = distmat[np.triu_indices_from(distmat, k=1)]
+    #             angle = np.mean(abs(flattened))
+    #             angles.append(angle)
+    #             iters += 1
 
-        if i == '15':
-            np.save('storeangles15_flat.npy', np.copy(angles))
-        else:
-            np.save('storeangles30_flat.npy', np.copy(angles))
+    #     if i == '15':
+    #         np.save('storeangles15_flat.npy', np.copy(angles))
+    #     else:
+    #         np.save('storeangles30_flat.npy', np.copy(angles))
 
-    ####
+    # ####
     # a15 = np.load('storeangles15_flat.npy')
     # a30 = np.load('storeangles30_flat.npy')
     # np.mean(a15)
     # np.mean(a30)
 
+    # t = range(len(a15[:100001]))
+    # plt.plot(t,savgol_filter(a15[:100001], 10000, 2),color='red')
+    # plt.plot(t,savgol_filter(a15[100001:200002], 10000, 2),color='red')
+    # plt.plot(t,savgol_filter(a15[200002:], 10000, 2),color='red')
+    # plt.plot(t,savgol_filter(a30[:100001], 10000, 2),color='blue')
+    # plt.plot(t,savgol_filter(a30[100001:200002], 10000, 2),color='blue')
+    # plt.plot(t,savgol_filter(a30[200002:], 10000, 2),color='blue')
 
     # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    # ax.plot(theta, r)
+    # ax.plot(t, a15,'o')
     # ax.set_rmax(2)
     # ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
     # ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
